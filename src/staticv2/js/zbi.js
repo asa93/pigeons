63d9cb469ef46413b0309c32430dfcf619aeb938
Slim <slimane.asa@outlook.com>
2021-10-08 16:58:22 +0200

    #presale 6h

--- src/staticv2/js/zbi.js
+++ src/staticv2/js/zbi.js
@@ -1,6 +1,6 @@
 exports.default = {
 
-    prsl_time : 2//ms
+    prsl_time : 6//h
     ,wl: [
         "FcLhxqudFpvqFwMtRdiciQLaFavyz3jc5Eyc3vEg9SVn"
         ,"5PHouKhmv7i2KE61Wv5kYG39icXqjMDse8UP9cck5FH4"
