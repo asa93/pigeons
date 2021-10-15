b41f7da343613349134b6bef4b3b7ccb6c72d075
Slim <slimane.asa@outlook.com>
2021-10-15 15:11:12 +0200

    trim

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,18 @@
-import Head from 'next/head'
-
-import { useState } from "react";
-import { Toaster } from 'react-hot-toast';
-import { useWallet } from "@solana/wallet-adapter-react";
-
-import {
-  shortenAddress,
-} from "../utils/candy-machine";
-import useCandyMachine from '../hooks/use-candy-machine';
-import useWalletBalance from '../hooks/use-wallet-balance';
-import Countdown from 'react-countdown';
-import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 import Static from '../staticv2/static_index';
 
 const Home = () => {
-  const [balance] = useWalletBalance()
-  const [isActive, setIsActive] = useState(false); // true when countdown completes
-  const wallet = useWallet();
+  //const [balance] = useWalletBalance()
+ // const [isActive, setIsActive] = useState(false); // true when countdown completes
+  //const wallet = useWallet();
 
-  const { isSoldOut, mintStartDate, isMinting, onMint, onMintMultiple, nftsData } = useCandyMachine()
+  //const { isSoldOut, mintStartDate, isMinting, onMint, onMintMultiple, nftsData } = useCandyMachine()
 
+  console.log("[RENDER] Home")
   return  <Static/>
 };
 
-const renderCounter = ({ days, hours, minutes, seconds, completed }: any) => {
-  return (
-    <span>
-      {hours} hours, {minutes} minutes, {seconds} seconds
-    </span>
-  );
-};
+ 
 
 export default Home;
 
