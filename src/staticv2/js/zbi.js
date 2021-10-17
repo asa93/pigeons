1acaeb18faf050b14c7b3eee89a1b57b3b054f27
Slim <slimane.asa@outlook.com>
2021-10-17 14:50:56 +0200

    whitelist

--- src/staticv2/js/zbi.js
+++ src/staticv2/js/zbi.js
@@ -3,7 +3,13 @@ exports.default = {
     prsl_time : 6//h
     ,wl: [
         "FcLhxqudFpvqFwMtRdiciQLaFavyz3jc5Eyc3vEg9SVn"
-        ,"5PHouKhmv7i2KE61Wv5kYG39icXqjMDse8UP9cck5FH4"
+        // ,"5PHouKhmv7i2KE61Wv5kYG39icXqjMDse8UP9cck5FH4"
+        // ,"7HXxb18eVxUun22D4JBAJh4axLP6FTdoAnAs3VLmmfJR"
+        // ,"59Ea8oCiDQEi8G9gXwhuwAfpSdgECHGwpFrsrs6Wwgxi"
+        // ,"4PpgL6rxAp7sE4JNGqN4dLHgfeBWS1z2Xg76dMMcmaDh"
+        // , "GTpWBJ1ksHbsch1AxtRe1zksBFa3Ctin1cz8aQbEXGdf"
+        // ,"For287UsT8dMbjxSC3NBs868RJ21rb3rGQPoc6fBB5aJ"
+        ,"Dk8CBi8v31amfsgaijKQEaXkKg1KF3s8eHsoLuPhfDFR"
     ]
 }
 
